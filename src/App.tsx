0c848c889b56147ce68a327d2ccab95176603008
Nazar Rachun <nazarrachun@gmail.com>
2023-06-05 15:08:20 +0300

    REG-4 | add 1 random object into the list via button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [randomItem, setRandomItem] = useState();
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
@@ -12,9 +13,26 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+        JSON.stringify(
+          setRandomItem(json[Math.floor(Math.random() * json.length)])
+        );
+      });
+  }, [randomItem]);
+
+  const handleGenerateItem = () => {
+    randomItem && setItems([randomItem, ...items]);
+  };
+
   return (
     <>
       <header className="App-header">
+        <button className="btn" onClick={handleGenerateItem}>
+          Generate item
+        </button>
         {items.map((item, index) => {
           return <p key={index}>{JSON.stringify(item)}</p>;
         })}
