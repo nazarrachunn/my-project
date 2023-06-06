dcedeb647c869a6126b7ae85b4178ea483b90a52
Nazar Rachun <nazarrachun@gmail.com>
2023-06-06 13:23:56 +0300

    deleted comments, import, double quotes, removed JSON.stringify from executed scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [randomItem, setRandomItem] = useState();
+  const [randomItem, setRandomItem] = useState('');
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch('https://jsonplaceholder.typicode.com/todos')
       .then((response) => response.json())
       .then((json) => {
-        setRandomItem(json[Math.floor(Math.random() * json.length)]);
+        setRandomItem(
+          JSON.stringify(json[Math.floor(Math.random() * json.length)])
+        );
       });
   }, []);
 
   return (
     <>
       <header className="App-header">
-        {<p>{JSON.stringify(randomItem)}</p>}
+        <p>{randomItem}</p>
       </header>
     </>
   );
