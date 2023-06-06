3bf8a82486f1e04fb13fb2b134f029c5ba83d9d9
nazarrachunn <87658481+nazarrachunn@users.noreply.github.com>
2023-06-06 20:54:00 +0300

    REG-4 | added random object into the list (#4)
    * REG-2 | render 1 random object
    
    * REG-3 | rendered list of objects
    
    * REG-4 | add 1 random object into the list via button
    
    * removed dependencies, useState, useEffect for randomItem

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,16 @@ function App() {
       .then(setItems);
   }, []);
 
+  const handleGenerateItem = () => {
+    setItems([...items, { id: 4, title: 'et porro tempora' }]);
+  };
+
   return (
     <>
       <header className="App-header">
+        <button className="btn" onClick={handleGenerateItem}>
+          Generate item
+        </button>
         {items.map((item) => {
           return <p key={item.id}>{item.title}</p>;
         })}
