f0cede185c346cbf7ce3a70ff166b77545bdc7f7
Nazar Rachun <nazarrachun@gmail.com>
2023-06-14 16:57:14 +0300

    update packages, deleted propstype

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,5 @@
 type Todo = { id: number; title: string };
 export type TodoArray = [number, string];
-export type Props = { todos: Todos };
 export type Todos = TodoArray[];
 
 export default Todo;
